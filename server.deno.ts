b6cb32405d2254994f273c776a3108953954aa1b
AkaishiTakumi <115193708+AkaishiTakumi@users.noreply.github.com>
2025-08-22 10:48:20 +0900

    👍 スレッドデータのオブジェクトを返すように変更
    ThreadModel: スレッドデータのオブジェクト

--- server.deno.ts
+++ server.deno.ts
@@ -3,6 +3,12 @@ import { UUID } from "npm:uuidjs";
 
 import newsList from "./public/news.json" with {type:"json"};
 
+type ThreadModel = {
+	"uuid": string;
+	"title": string;
+	"summary": string | null;
+};
+
 Deno.serve(async (req: Request) => {
 	const pathname: string = new URL(req.url).pathname;
 
@@ -23,21 +29,24 @@ Deno.serve(async (req: Request) => {
 			"createdAt": null,
 		});
 
+		const objectList: ThreadModel[] = [];
+
 		// Deno KVに保存
 		// 第一引数はkey, 第二引数はvalue
 		// keyが既に存在する場合は、更新
 		const selectedTitles: string[] = shuffled.slice(5);
 		for await (const news of selectedTitles) {
 			const threadUUID: string = UUID.generate();
-			await kv.set([newsUUID, threadUUID], {
+			objectList.push({
 				"uuid": threadUUID,
 				"title": news,
 				"summary": null,
 			});
+			await kv.set([newsUUID, threadUUID], objectList.at(-1));
 		}
 
 		// listをJSONとして返す
-		return new Response(JSON.stringify(selectedTitles), {
+		return new Response(JSON.stringify(objectList), {
 			headers: { "Content-Type": "application/json" },
 		});
 	}
