8c35c354c952b6f1568b39672ae671fb65c2451c
yuma-harada <yu2ma1da7ha0ra@gmail.com>
2025-08-27 11:04:18 +0900

    🎨: APIにコメントアウトを追加

--- test.server.deno.ts
+++ test.server.deno.ts
@@ -4,18 +4,34 @@ import { getThreadTitles } from "./api/threadList.ts";
 import { createThreadPosts, getThreadPosts } from "./api/threadContent.ts";
 import { createThreadSummary } from "./api/newspaperContent.ts";
 
-// Honoアプリケーションの初期化
 const app = new Hono();
 
-// スレッドタイトル取得API
+/**
+ * スレッドのタイトル一覧を取得するAPI
+ */
 app.get("/thread-titles", getThreadTitles);
 // スレッド投稿取得API
+
+/**
+ * スレッドの投稿一覧を取得するAPI
+ * @property threadId - スレッドが持っているUUID
+ */
 app.get("/thread-posts", getThreadPosts);
 
 // テスト用データ作成API
+
+/**
+ * テスト用のダミー投稿データを作成するAPI
+ * @property newspaperId - 新聞が持っているUUID
+ * @property index - ダミー投稿を作成したいスレッドの新聞内のスレッド番号(リストの順番)(0 ~ 4)
+ */
 app.get("/create-posts", createThreadPosts);
 
-// スレッド要約API
+/**
+ * スレッドの投稿内容を新聞の記事風に要約生成するAPI
+ * @property newspaperId - 新聞が持っているUUID
+ * @property index - 投稿要約を作成したいスレッドの新聞内のスレッド番号(リストの順番)(0 ~ 4)
+ */
 app.post("/thread-summary", createThreadSummary);
 
 // 静的ファイル配信
