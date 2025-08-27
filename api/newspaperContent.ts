9055260a875dae6d1470fd8739e5bb42dfc8b686
AkaishiTakumi <115193708+AkaishiTakumi@users.noreply.github.com>
2025-08-27 16:55:24 +0900

    👍 summaryを取得

--- api/newspaperContent.ts
+++ api/newspaperContent.ts
@@ -13,7 +13,8 @@ const createThreadSummary = async (ctx: Context) => {
 
     const requestJson = await ctx.req.json();
     const newspaperId: string = requestJson.uuid;
-    const index: number = requestJson.index;
+    const indexStr: string = requestJson.index;
+    const index: number = Number(indexStr);
 
     const kv: Deno.Kv = await Deno.openKv();
     const threadData: Deno.KvEntryMaybe<ThreadModel> = await kv.get([newspaperId, index]);
